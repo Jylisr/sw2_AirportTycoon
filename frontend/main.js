7fcb57d39b628abf3bae96f67ad861e33fd83439
Juho <juho.ylisir@gmail.com>
2023-12-16 16:10:30 +0200

    minor configurations

--- frontend/main.js
+++ frontend/main.js
@@ -10,8 +10,7 @@ map.setView([60, 24], 7);
 
 // global variables
 const apiUrl = 'http://127.0.0.1:5000/';
-const startLoc = 'EFHK';
-const globalGoals = [];
+const startLoc = 'Helsinki Vantaa Airport';
 const airportMarkers = L.featureGroup().addTo(map);
 
 // icons
@@ -28,6 +27,48 @@ async function getData(url) {
   return data;
 }
 
+
+document.querySelector('#player-form').addEventListener('submit', function (evt) {
+  evt.preventDefault(); // player must enter name
+  console.log("form is supposed to show itself")
+  let playerName = document.querySelector('#player-input').value;
+  document.querySelector('#player-modal').classList.add('hide'); // hide form after
+  document.querySelector('#player-name').innerHTML = `Player: ${playerName}`;
+});
+
+let playerName = document.querySelector('#player-name').innerHTML
+let str=""
+for (let i = 0; i < playerName.length; i++)
+{
+  str += playerName[i]
+  playerName = str
+}
+
+function shop_supreme(Shopdata,total_budget) {
+  function showform() {
+    document.querySelector('#shop-modal').classList.remove('hide');
+  }
+    function submit(evt) {
+    evt.preventDefault();
+    console.log("Form submitted");
+    let shop_name = document.querySelector('#shop-name').value;
+    for (let [shopname,shopnums] of Object.entries(shopData))
+      if (shop_name == shopname){
+        console.log("Player has bought "+shopname+` for ${shopnums[0]}`)
+        document.querySelector('#store-shop').innerText = shop_name
+    // Perform any additional actions with playerName
+        document.querySelector('#shop-modal').classList.add('hide'); // hide form after
+        document.querySelector('#money').innerHTML = total_budget - shopnums[0];
+        return shopnums
+      }
+  }
+
+  let shopname = document.querySelector('#store-shop').innerText
+  document.querySelector('#buybutton').addEventListener('click', showform);
+  document.querySelector('#shop-modal').addEventListener('submit', submit);
+
+}
+
 // function to update game status
 function updateStatus(status) {
   document.querySelector('#player-name').innerHTML = `Player: ${status.name}`;
@@ -37,43 +78,49 @@ function updateStatus(status) {
 }
 
 // function to show weather at selected airport
-function showShops(shops) {
-  for (let shop of shops) {
-    document.querySelector('#consumed').innerHTML = `Weather at ${airport.name}`;
-  document.querySelector('#budget').innerHTML = `${airport.weather.temp}°C`;
-  document.querySelector('#money').src = airport.weather.icon;
-  document.querySelector('#player-name').innerHTML = airport.weather.description;
-  document.querySelector('#airport-wind').innerHTML = `${airport.weather.wind.speed}m/s`;
-  }
+function showShops(shop) {
+  let shops= {}
+    document.querySelector('#shop1').innerHTML = `${shop[0][shopname]}`;
+    shops[shop[0][shopname]] = [shop_price, shop_revenue]
+ 
+  document.querySelector('#shop2').innerHTML = `${shop[1][shopname]}`;
+   shops[shop[1][shopname]] = [shop_price, shop_revenue]
+ 
+  document.querySelector('#shop3').innerHTML = `${shop[2][shopname]}`;
+   shops[shop[2][shopname]] = [shop_price, shop_revenue]
+
+  document.querySelector('#shop4').innerHTML = `${shop[3][shopname]}`;
+   shops[shop[3][shopname]] = [shop_price, shop_revenue]
+ 
+  document.querySelector('#shop5').innerHTML = `${shop[4][shopname]}`;
+   shops[shop[4][shopname]] = [shop_price, shop_revenue]
+ 
+  document.querySelector('#shop6').innerHTML = `${shop[5][shopname]}`;
+   shops[shop[5][shopname]] = [shop_price, shop_revenue]
+ 
+  document.querySelector('#shop7').innerHTML = `${shop[6][shopname]}`;
+   shops[shop[6][shopname]] = [shop_price, shop_revenue]
+ 
+  document.querySelector('#shop8').innerHTML = `${shop[7][shopname]}`;
+   shops[shop[7][shopname]] = [shop_price, shop_revenue]
+  console.log("Shops Generated")
+  return shops
 }
 
-
-// function to update goal data and goal table in UI
-function updateGoals(goals) {
-  document.querySelector('#goals').innerHTML = '';
-  for (let goal of goals) {
-    const li = document.createElement('li');
-    const figure = document.createElement('figure');
-    const img = document.createElement('img');
-    const figcaption = document.createElement('figcaption');
-    img.src = goal.icon;
-    img.alt = `goal name: ${goal.name}`;
-    figcaption.innerHTML = goal.description;
-    figure.append(img);
-    figure.append(figcaption);
-    li.append(figure);
-    if (goal.reached) {
-      li.classList.add('done');
-      globalGoals.includes(goal.goalid) || globalGoals.push(goal.goalid);
-    }
-    document.querySelector('#goals').append(li);
-  }
-}
 
 // function to check if game is over
 function checkGameOver(budget, money) {
-  if (budget <= 0 || money <= 0){
-    alert(`Game Over. ${globalGoals.length} goals reached.`);
+  if (budget <= 0)
+  {
+    document.querySelector('#consumed').innerHTML = 10000
+    document.querySelector('#budget').innerHTML = 0
+    document.querySelector('#goal').classList.remove('hide');
+    console.log("Game over protocols accomplished")
+    return false;
+  }
+  else if (money <= 0){
+    document.querySelector('#money').innerHTML = money;
+    document.querySelector('#goal').classList.remove('hide');
     return false;
   }
   return true;
@@ -85,26 +132,21 @@ async function gameSetup(url) {
   try {
     document.querySelector('.goal').classList.add('hide');
     airportMarkers.clearLayers();
-    /*const highscorelist = await getData('http://127.0.0.1:5000/highscore');*/
     const gameData = await getData(url)
-    if (gameData)
-    {
-      console.log("Somethings evidently wrong")
-    }
-    else
-    {
-      console.log("Somethings still evidently wrong")
-    }
     updateStatus(gameData.status);
     const Locations = await getData('http://127.0.0.1:5000/flyrequest')
     if (!checkGameOver(gameData.status.co2_budget, gameData.status.money)) return;
+    const Shops = await getData("http://127.0.0.1:5000/shoprequest")
+    var ShopData = showShops(Shops);
+    let revenue= 0
+    money,revenue = shop_supreme(ShopData)
     for (let airport of Locations) {
       const marker = L.marker([airport.latitude, airport.longitude]).addTo(map);
       airportMarkers.addLayer(marker);
       if (airport.active) {
         map.flyTo([airport.latitude, airport.longitude], 10);
         const Shops = await getData("http://127.0.0.1:5000/shoprequest")
-        showShops(Shops);
+        var ShopData = showShops(Shops);
         marker.bindPopup(`You are here: <b>${airport.name}</b>`);
         marker.openPopup();
         marker.setIcon(greenIcon);
@@ -127,7 +169,6 @@ async function gameSetup(url) {
         });
       }
     }
-    updateGoals(gameData.goals);
   } catch (error) {
     console.log(error);
   }
